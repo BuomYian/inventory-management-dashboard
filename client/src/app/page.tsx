5f3d54057e0fbc6d1c10c364cef2e4eb3dc5bf0b
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-09-26 15:48:59 +0300

    configured tailwind file

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,5 @@
+import Dashboard from "./Dashboard/page";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello there
-    </main>
-  );
+  return <Dashboard />;
 }
